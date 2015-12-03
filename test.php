3de2306916cfa77696e2e309951ae780d059b213
root <root@ShreyPrajapati.(none)>
2015-12-03 19:55:36 +0530

    test Revert "first commit on new repo"
    This reverts commit d510e58552f77d5ba95ed21d2b58d8f4c194d2e8.
    
    Conflicts:
    
    	test.php

--- test.php
+++ test.php
@@ -24,3 +24,4 @@ echo "Hi this is first file in root folder";
 echo "Hi this is first file in root folder";
 echo "Hi this is first file in root folder";
 echo "Hi this is first file in root folder";
+echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";
