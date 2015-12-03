8275bac0aeeeea441ee06c6a51f565857244f74f
root <root@ShreyPrajapati.(none)>
2015-12-03 20:05:13 +0530

    test

--- test/test.php
+++ test/test.php
@@ -24,5 +24,3 @@ echo "Hi this is first file in root folder";
 echo "Hi this is first file in root folder";
 echo "Hi this is first file in root folder";
 echo "Hi this is first file in root folder";
-echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";
-echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";echo "Hi this is first file in root folder";
---
+++ test1.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Hi this is first test1.php from New";
